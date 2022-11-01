88ca63e2f7493edd44585907b3143358d6399912
Julien Adam <julien.adam@gmail.com>
2022-11-01 13:29:25 +0100

    Test de recette en place

---
+++ Kata.BankAccount.Tests/CompteRecette.cs
@@ -0,0 +1,25 @@
+using System;
+using Moq;
+using Xunit;
+
+namespace Kata.BankAccount.Tests;
+
+public class CompteRecette
+{
+    [Fact]
+    public void L_extrait_de_compte_affiche_les_transactions_dans_l_ordre_chronologique_inverse()
+    {
+        var console = new Mock<Terminal>();
+        var account = new Compte();
+        account.Deposer(1000);
+        account.Retirer(100);
+        account.Deposer(500);
+
+        account.AfficherExtrait();
+
+        console.SetupSequence(c => c.AfficherLigne("DATE | MONTANT | SOLDE"));
+        console.SetupSequence(c => c.AfficherLigne("27/08/2021 | 500.00 | 1400.00"));
+        console.SetupSequence(c => c.AfficherLigne("06/08/2021 | -100.00 | 900.00"));
+        console.SetupSequence(c => c.AfficherLigne("01/08/2021 | 1000.00 | 1000.00"));
+    }
+}
---
+++ Kata.BankAccount/Terminal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.BankAccount
+{
+    public class Terminal
+    {
+        public void AfficherLigne(string ligne)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
